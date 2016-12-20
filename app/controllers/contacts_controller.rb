40a7c1de4ca0d51e16709d8f20cd264f063afcab
disne_y <bold.disney@gmail.com>
2016-12-20 15:14:00 +0300

    Add contact model validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,13 @@ class ContactsController < ApplicationController
   end
   
   def create
-    @contact = Contact.new(contact_params)   
-    @contact.save
+    @contact = Contact.new(contact_params)  
+    if @contact.valid?
+      @contact.save
+    else
+      render action: 'new'
+    end 
+    
   end
 
   private
