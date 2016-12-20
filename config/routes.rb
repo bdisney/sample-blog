d95cb513a81eebc7af6ee4b641e9c3ecb056f86e
disne_y <bold.disney@gmail.com>
2016-12-20 19:16:25 +0300

    Add terms controller, Save article on submit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'contacts' => 'contacts#new' первый способ вывода страницы при обновлении
   resources :articles
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
+  resource :terms, only: [:show], path_names: {:show => ''}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
