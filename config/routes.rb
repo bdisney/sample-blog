9c7ddac20b9d145c36b635a8ed1bcbf7f769ea1e
disne_y <bold.disney@gmail.com>
2016-12-21 19:58:56 +0300

    Add comments to post, Display comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
 
   # get 'contacts' => 'contacts#new' первый способ вывода страницы при обновлении
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
