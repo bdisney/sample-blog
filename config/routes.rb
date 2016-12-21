688e12e965a43cd12a86f8e946a6a4cd584b547f
disne_y <bold.disney@gmail.com>
2016-12-21 14:01:26 +0300

    Create pages controller. Add about & terms pages to pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   # get 'contacts' => 'contacts#new' первый способ вывода страницы при обновлении
   resources :articles
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resource :terms, only: [:show], path_names: {:show => ''}
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
