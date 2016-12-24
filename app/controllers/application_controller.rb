6e25511ef1440bfb8422ad7662cfc041ed9a911c
disne_y <bold.disney@gmail.com>
2016-12-24 21:04:06 +0300

    Permit username for sign up, sign in, account_update forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username]) # Синтакс в rails5 немного отличается 
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+
+  end 
+
 end
