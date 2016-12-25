47d272731df2e4171ded7e944a5411de7a5a3a73
disne_y <bold.disney@gmail.com>
2016-12-25 14:40:59 +0300

    Add username to displayed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,16 @@ before_filter :authenticate_user!, only: [:create]
 
 def create
   @article = Article.find(params[:article_id])
-  @article.comments.create(comment_params)
+  @new_comment = @article.comments.new(comment_params)
+  @new_comment.user_id = current_user.id
+  @new_comment.save!
   
   redirect_to article_path(@article)
 end
 
 private
   def comment_params
-      params.require(:comment).permit(:author, :body)
+      params.require(:comment).permit(:user_id, :body)
   end
 
 end
