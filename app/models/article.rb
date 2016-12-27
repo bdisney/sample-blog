9d51a0420c4f24dda942a08553f0787e8d7de7ff
disne_y <bold.disney@gmail.com>
2016-12-27 22:31:27 +0300

    Add subject method for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
   has_many :comments
-  belongs_to :user
+  belongs_to :user, optional: true # опшионал тру добавлено для прохождения теста
   
   validates :title, presence: true
   validates :text, presence: true
+
+  def subject
+    title
+  end
 end
